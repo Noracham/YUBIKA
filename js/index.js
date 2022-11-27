e5bbbfed36f8d08f0cf9c3baf1bece0fcd7d8c7b
Nora <nsorym04@gmail.com>
2022-11-27 22:24:35 +0900

    pointレスポンシブ

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// $(function(){
+//     // $(window).on("load",function(){
+//         if($(window).width() <= 768){
+    
+//             $("#change_a").before($("#change_b"));
+//         }
+//         else{
+//             $("#change_b").before($("#change_a"));
+//         }
+//     })
+// // })
