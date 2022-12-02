d505debed528cf52bc777c0876992538a8f181c7
Nora <nsorym04@gmail.com>
2022-12-02 12:17:10 +0900

    当社の強みリンク

---
+++ js/fadeIn.js
@@ -0,0 +1,6 @@
+$(function(){
+      //loading
+  $(window).on("load",function(){
+    $("#fade_hid").delay(500).fadeIn(1000);
+  })
+})
--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-$(function(){
-    $(".op_menu").on("click",function(){
-       $(this).toggleClass("active");
+$(function () {
+  $(".op_menu").on("click", function () {
+    $(this).toggleClass("active");
 
-       if($(this).hasClass("active")){
-        $(".menu_screen").animate({right:"0"},500);
-    }else{
-        $(".menu_screen").animate({right:"-100vw"},500);
+    if ($(this).hasClass("active")) {
+      $(".menu_screen").animate({ right: "0" }, 500);
+    } else {
+      $(".menu_screen").animate({ right: "-100vw" }, 500);
     }
-    })
-})
+  });
+});
