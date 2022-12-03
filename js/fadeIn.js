94e0fe7ab1353339c0d47f2d4d925fad210b46c3
Nora <nsorym04@gmail.com>
2022-12-03 16:09:29 +0900

    ローディング完成

--- js/fadeIn.js
+++ js/fadeIn.js
@@ -1,11 +1,7 @@
 $(function(){
       //loading
-      Pace.on('done', function(){
-        $('.pace-running .hide').fadeIn(1000);
-    });
-
-
-//   $(window).on("load",function(){
-//     $(".fade_hid").delay(500).fadeIn(1000);
-//   })
+      $(window).on("load",function(){
+        $("#loading").addClass("loaded");
+        $(".hide").css("display","block");
+      });
 })
